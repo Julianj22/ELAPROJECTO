2ccac53238a0a15d3ea489fe8b85cb16cc2bdf9b
Julianj22 <42346111+Julianj22@users.noreply.github.com>
2018-12-17 08:56:40 -0500

    Add files via upload

---
+++ brain.js
@@ -0,0 +1,251 @@
+console.log("loaded!");
+
+
+let play = document.querySelector("#player");
+
+
+// From first platform to the second using the platform 1 box
+
+let x = document.querySelector('#p1_box');
+ x.addEventListener('collide', e => {
+play.setAttribute("position","48.332 5 6");
+});
+
+//From second platform to first tier using the platfrom 2 box
+
+let xx = document.querySelector('#p2_box');
+ xx.addEventListener('collide', e => {
+play.setAttribute("position","-24 146 -70");
+});
+
+//From the first tier to the second tier using the tier 1 box
+
+let xxx = document.querySelector('#t1_box');
+ xxx.addEventListener('collide', e => {
+play.setAttribute("position","-36 221 40");
+});
+
+//From the second tier to the third tier using the tier 2 box
+
+let xxxx = document.querySelector('#t2_box');
+ xxxx.addEventListener('collide', e => {
+play.setAttribute("position","47.23 271 109.889");
+
+});
+let xxxxx = document. querySelector('#t3_box');
+ xxxxx.addEventListener('collide', e => {
+  play.setAttribute("position", "29.022 10 26.599");
+ })
+
+//Death plane using the main plane
+let p = document.querySelector('#mp');
+ p.addEventListener('collide', e => {
+play.setAttribute("position","12.183 0 3");
+});
+let bb = document.querySelector("#boxbrain")
+ bb.addEventListener("collide", myFunction);
+ bb.addEventListener("collide", secfunction);
+ bb.addEventListener("collide", thirdfunction);
+ bb.addEventListener("collide", forfunction);
+ bb.addEventListener("collide", fiffunction);
+ bb.addEventListener("collide", sixfunction);
+ bb.addEventListener('collide', sevfunction);
+ function myFunction(){
+
+alert("The amygdala stores memories of frightening events and other emotional experiences. In people with anxiety disorders, the amygdala may be so sensitive that it overreacts in situations that aren't threatening. From this we can tell the amygdala is responsible for holding Holden's memory of Allies death which can be a trigger for his anxiety. This can be shown as said 'My hand still hurts me once in a while, when it rains and all, and I can't make a real fist anymore-not a tight one'(Salinger 39)" );
+//Obtain that grain edwin. Lets get this bread
+}//yo, we need an explanation of our diagnosis. wait, could what you put for each brain part count? yeah lets just put in the conclusion thats why he has anxiety i mean afterall ive been using anxiety throughout all of them ok cool, one more fuction then
+function secfunction(){
+alert("The hippocampus, has a central role in processing emotions and long-term memories. Research has found that the hippocampus is smaller than normal in some people who were abused as children, an experience that increases the risk for post-traumatic stress disorder and other anxiety disorders.This makes it clear that the hippocampus is responsible for Holden being depressed and even wanting to kill himself as said in the text'What I really felt like, though, was committing suicide.I felt like jumping out the window.(Salinger 104)'")
+}
+function thirdfunction(){
+alert(" The locus ceruleus is an area of the brainstem that helps determine which brain stimuli are worth paying attention to. When the locus ceruleus was electronically stimulated, it's displayed anxiety-like symptoms. That proves the locus ceruleus can be responsible for Holden's anxiety like symptoms such as unwanted thought as said 'I kept laying there in the dark anyway, though, trying not to think about old Jane and Stradlater...But it was almost impossible'(Salinger 46) or even the unwanted thoughts of suicide such as'I felt so lonesome, all of a sudden. I almost wished I was dead'(Salinger 45).")
+}
+function forfunction(){
+alert(" The prefrontal cortex is involved in making decisions, solving problems, and exercising judgment. It also appears to have a hand in storing memories of extinguishing fears which might occur during treatment for an anxiety disorder and turning down the fear response. Obviously Holden's adolescent prefrontal cortex is to blame for his irational decisions and his lack of fear in all countless situations such as running away as said 'Finally, what i decided I'd do I decided I'd go away. I decided I'd never go home again and I'd never go away to another school again.'(Salinger 198)  ")
+}
+function fiffunction(){
+alert("The amygdala and the hippocampus both work together in making emotionally driven memories so powerful. For Holden however, this may serve as a rather deleterious relationship for him to live with. Two coexisting entities that's natural function results in Holden's triggering reminiscence (quote 1) and wanting to commit suicide(quote 2) on occasions where there happens to be an influx of powerful, overwhelming emotions. As illustrated by the aftermath of his, presumably, first encounter with death(The death of Allie), what we feel and think to ourselves can often transcend into physical actions and events. And as these maifested emotions begin to swelter in the mind of someone with generalized anxiety disorder, thoughts of an existencial exodus could ultimately become an object of desire(Holden pondering about his own death.).   ")
+}
+function sixfunction(){
+alert("Like the amygdala and hippocampus, the locus ceruleus and prefrontal cortex could also share an unlikely bond. But for Holden, this could be nothing other than a'love-hate' relationship. Decisions made by the locus ceruleus such as the revival of unwanted memories that include Jane(quote 3) and Holden's own morbid isolation(quote4), often interject with one's own concious outlook as utilized by the prefrontal cortex. With this feeling of not being able to control your own thoughts, let alone your own life, a person with generalized anxiety disorder(Or even an adolescent) could act rebellious towards even their own interests to feel some sort of independence, or to have something they beleive to be their own. Holden's recurring dreams of distancing himself from school and family(quote 5) is an indicator that Holden does, like many, yearn for salvation far from where it is actually hidden. Within ourselves.")
+}
+function sevfunction(){
+ alert("Holden has generalized anxiety disorder because of the excessive amount of stress he places on the past in relation to Allie and his personal experiences with loss, hardship and grief. In additon, Holden shows signs of having GAD when contemplating death. The amount of focus he gives to thoughts and emotions that prove to be more damaging than not(Jane and isolation), is a major contibutor to his suicidal notions. In conclusion, we diagnos Holden Caufeild with Generalized Anxiety Disorder. ")
+}
+
+alert("Edwin and Julian's ELA Brain Diagram Project");
+
+//ok, the analysis is finished. I need to go to bed but i submitted the checklist already so no worries their. i guess just finish up your java script and we'll be done. if not, then i'll just bring my laptop tomorrow during mr. brin's free peroid.
+// lol bro i never get sleep so it dont matter how late i stay up
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
+// lets get this bread
